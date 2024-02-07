4ebccb4689276d89a50615625e61cce81aa14ecb
Kamil <kamilmomunjanov@gmail.com>
2024-02-07 16:20:24 +0600

    Add Profile Quote Api

--- src/Components/StockList/StockItem/StockItem.jsx
+++ src/Components/StockList/StockItem/StockItem.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 import style from "./style.module.css";
+import stockApi from "../../../api/stockApi";
+import {useQuery} from "react-query";
+
+const fetchStock = async (symbol) => {
+    const price = await  stockApi.getPrice(symbol)
+    const profile = await  stockApi.getProfile(symbol)
+
+    const stock = {...profile, price: price.high}
+    return stock
+}
 
 const StockItem = ({stock, deleteFromFavorite}) => {
+    const {data, isLoading} = useQuery(`getStock/${stock.symbol}`, () => fetchStock(stock.symbol))
+    if (data && !isLoading) {
+        console.log(data);
+    }
+
     return (
         <li className={style.items}>
             <p>{stock.name}</p>
--- src/api/stockApi.js
+++ src/api/stockApi.js
@@ -1,21 +1,59 @@
-import axios from "axios";
-const BASE_URL = "https://api.twelvedata.com/stocks"
+    import axios from "axios";
+    const BASE_URL = "https://api.twelvedata.com/stocks"
+    const BASES_URL = "https://api.twelvedata.com"
+    const API_KEY = "36fb6d1f66f64b92869d52f8296916a9"
+    // https://api.twelvedata.com/profile?symbol=AAPL&apikey=36fb6d1f66f64b92869d52f8296916a9
+    // https://api.twelvedata.com/quote?symbol=AAPL&apikey=36fb6d1f66f64b92869d52f8296916a9
 
-const stockApi = {
-    getStocks: async function () {
-        try {
-            const response = await axios.get(`${BASE_URL}`);
-            return response.data;
-        } catch (error) {
-            console.error("Error fetching stocks:", error);
-            throw error;
+    const stockApi = {
+        getStocks: async function () {
+            try {
+                const response = await axios.get(`${BASE_URL}`);
+                return response.data;
+            } catch (error) {
+                console.error("Error fetching stocks:", error);
+                throw error;
+            }
+
+        },
+        getProfile: async function (symbol) {
+            try {
+                if (typeof symbol !== 'string') {
+                    throw new Error('Invalid symbol provided');
+                }
+                const response = await axios.get(`${BASES_URL}/profile`, {
+                    params: {
+                        symbol: symbol,
+                        apikey: API_KEY
+                    }
+                });
+                return response.data;
+            } catch (error) {
+                console.error("Error fetching stocks:", error);
+                throw error;
+            }
+        },
+        getPrice: async function (symbol) {
+            try {
+                if (typeof symbol !== 'string') {
+                    throw new Error('Invalid symbol provided');
+                }
+                const response = await axios.get(`${BASES_URL}/quote`, {
+                    params: {
+                        symbol: symbol,
+                        apikey: API_KEY
+                    }
+                });
+                return response.data;
+            } catch (error) {
+                console.error("Error fetching stocks:", error);
+                throw error;
+            }
         }
-
     }
 
 
-}
 
 
 
-export default stockApi;
+    export default stockApi;
